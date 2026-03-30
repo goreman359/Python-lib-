e7678a26cdac93a7be956cbb1956d477b1e47449
krinitsa <rodnik.alex777@gmail.com>
2026-03-30 19:35:55 +0500

    основные математические функции

---
+++ math.py
@@ -0,0 +1,17 @@
+
+def add(a,b):
+    result = a + b
+    return result
+
+def subtract(a,b):
+    result = a - b
+    return result
+
+def multiply(a,b):
+    result = a * b
+    return result
+
+def divide(a,b):
+    if b == 0: print ("Ошибка. Деление на ноль."); return
+    result = a / b
+    return result
